aa5a6ff142a68f0e08f58259fa3438a148214a05
Yuan, Frank <FYuan@cvent.com>
2019-07-08 21:56:52 -0300

    Parameterize script

---
+++ findCamisSite.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# Customize these values
+# [Campsite, Roofed ... ]
+DOMAIN=$1
+SITE_TYPE=$2
+# [Banff, Fundy, ...]
+PARK=$3
+ARRIVAL=2019-$4
+NIGHTS=$5
+PARTY_SIZE=6
+#ELECTRICAL_SERVICE=&ElectricalService_incl=on
+
+# CONSTANTS
+BASE_URL=https://$1
+AVAILABLE="Available"
+COOKIE_PATH=/tmp/Cookie.$1
+COOKIE_CONTENT="CookieLocaleName=en-CA"
+DATA='resType='$SITE_TYPE'&partySize='$PARTY_SIZE'&equipment=&equipmentSub=&tentPads=&vehicleType=&Accessible_excl=on'$ELECTRICAL_SERVICE'&arrDate='$ARRIVAL'&nights='$NIGHTS'&apId=null&rceId=&bookingDisplay=0'
+
+curl -s --dump-header $COOKIE_PATH $BASE_URL/$PARK?List --cookie $COOKIE_CONTENT &>-
+RESULT=$(curl $BASE_URL'/ResInfo.ashx' -H 'Origin: '$BASE_URL -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: */*' -H 'Referer: '$BASE_URL/$PARK'?List' -H 'Connection: keep-alive' --data $DATA --compressed --cookie $COOKIE_PATH --cookie $COOKIE_CONTENT -s)
+RESULT=$(curl $BASE_URL'/view.ashx?async=true&sort=null&nav=undefined&selectedSitesOnly=false&selectedSites=' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: '$BASE_URL/$PARK'?List' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7' --compressed --cookie $COOKIE_PATH --cookie $COOKIE_CONTENT -s| grep -e "<img alt=\"$AVAILABLE\"")
+if [ $? -eq 0 ]; then
+	SITE=$(echo $RESULT | sed -e 's/<[^>]*>/|/g' | sed -E 's/\|+/, /g')
+	osascript -e "display notification \"Found a site on $ARRIVAL $SITE$PARK!\" with title \"Notification\""
+	say Site found in $PARK at $ARRIVAL
+	echo $RESULT
+fi
+
--- findMySite.sh
+++ findMySite.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
-COOKIE_PATH=/tmp/Cookie.reservation.pc.gc.ca
-COOKIE_CONTENT="CookieLocaleName=en-CA"
-PARK=Banff
-ARRIVAL=2018-08-01
-NIGHTS=1
-AVAILABLE="Available"
+#export https_proxy=http://127.0.0.1:8888
+DOMAIN=reservation.pc.gc.ca
+/Users/fyuan/Projects/FindParkSite/findCamisSite.sh $DOMAIN $1 $2 $3 $4
 
-curl -s --dump-header $COOKIE_PATH https://reservation.pc.gc.ca/$PARK?List --cookie $COOKIE_CONTENT &>-
-RESULT=$(curl 'https://reservation.pc.gc.ca/ResInfo.ashx' -H 'Origin: https://reservation.pc.gc.ca' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'Accept: */*' -H 'Referer: https://reservation.pc.gc.ca/$PARK?List' -H 'Connection: keep-alive' --data 'resType=Campsite&partySize=2&equipment=&equipmentSub=MediumTent&tentPads=&vehicleType=&Accessible_excl=on&ElectricalService_incl=on&arrDate='$ARRIVAL'&nights='$NIGHTS'&apId=null&rceId=&bookingDisplay=0' --compressed --cookie $COOKIE_PATH --cookie $COOKIE_CONTENT -s)
-RESULT=$(curl 'https://reservation.pc.gc.ca/view.ashx?async=true&sort=null&nav=undefined&selectedSitesOnly=false&selectedSites=' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: https://reservation.pc.gc.ca/$PARK?List' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7' --compressed --cookie $COOKIE_PATH --cookie $COOKIE_CONTENT -s| grep -e "<img alt=\"$AVAILABLE\"")
-if [ $? -eq 0 ]; then
-	SITE=$(echo $RESULT | sed -e 's/<[^>]*>/|/g' | sed -E 's/\|+/, /g')
-	osascript -e "display notification \"Found a site on $ARRIVAL $SITE$PARK!\" with title \"Notification\""
-	say Site found in $PARK at $ARRIVAL
-fi
